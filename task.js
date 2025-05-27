222c87a45972ec95c6f66b9e5d226be2fff2bc6e
kksahu0149 <krishnakumarsahu533@gmail.com>
2025-05-27 11:39:33 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const myButton = document.getElementById('myButton');
+  
+    myButton.addEventListener('click', function() {
+        alert('yayyy you are a APEXPLANET intern');
+        // You can add more JavaScript code here to perform other actions
+        console.log('Button was clicked at:', new Date());
+    });
+  });
